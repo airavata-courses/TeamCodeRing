164e954c9663489d5f914c99788b3bed8cf83570
Prashanth Murali <prashanthkumar374@gmail.com>
2016-09-17 20:25:33 -0400

    set flag for bundler

--- anemometer/config/deploy/production.rb
+++ anemometer/config/deploy/production.rb
@@ -72,6 +72,7 @@ set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
+set :bundle_flags, "--deployment"
 set :rvm_ruby_version, '2.2.4' # Edit this if you are using MRI Ruby
 
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
