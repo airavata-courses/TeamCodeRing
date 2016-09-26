c86e4d5267dc8792c828e6d4c9a7ebc1a424ec69
Prashanth Murali <prashanthkumar374@gmail.com>
2016-09-25 21:13:32 -0400

    Adding new SSH keypair

--- anemometer/config/deploy/production.rb
+++ anemometer/config/deploy/production.rb
@@ -72,7 +72,7 @@ set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
-set :ssh_options, keys: ["config/CodeRing.pem"]
+set :ssh_options, keys: ["config/deploy_id_rsa"]
 set :bundle_flags, "--deployment"
 set :rvm_ruby_version, '2.2.4' # Edit this if you are using MRI Ruby
 
