d6378170cd2dc4d42d2f424b177f78e8eaeab0fa
Prashanth Murali <prashanthkumar374@gmail.com>
2016-10-14 01:33:02 -0400

    s3 bucket URLs added
    1. Changed in data investor
    2. Made accommodations in anemometer

--- anemometer/app/controllers/pages_controller.rb
+++ anemometer/app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 require 'net/http'
+require 'json'
+
 class PagesController < ApplicationController
   def index
     @station_list = YAML.load(File.read("config/station.yml"))
@@ -7,13 +9,13 @@ class PagesController < ApplicationController
   def form_url
     uri = URI(RAIN_GAUGE[Rails.env])
     uri.query = URI.encode_www_form(:stations => params[:stations], :date => params[:start][:date])
-    res = Net::HTTP.get_response(uri)
-    if res.is_a?(Net::HTTPSuccess)
-      flash[:success] = "Data ingested, The data URL is : #{res.body}"
+    @data_results = Net::HTTP.get_response(uri)
+    if @data_results.is_a?(Net::HTTPSuccess)
+      flash[:success] = "Data ingestor invoked, here are the results..."
     else
       flash[:failure] = "Unable to get a Data URL"
+      redirect_to root_path
     end
-    redirect_to root_path
   end
 
 end
