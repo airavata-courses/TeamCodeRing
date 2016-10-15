61ec7f48b6eee3f864cce306e24676e1cbfd5d5d
necsashi <necsashi@indiana.edu>
2016-10-15 13:18:29 -0400

    adding redis to anemometer

--- anemometer/app/controllers/pages_controller.rb
+++ anemometer/app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 require 'net/http'
 require 'json'
-
+require 'redis'
 class PagesController < ApplicationController
 
   before_filter :require_current_user
@@ -10,11 +10,11 @@ class PagesController < ApplicationController
   end
 
   def form_url
-    uri = URI(RAIN_GAUGE[Rails.env])
-    uri.query = URI.encode_www_form(:stations => params[:stations], :date => params[:start][:date])
-    @data_results = Net::HTTP.get_response(uri)
-    if @data_results.is_a?(Net::HTTPSuccess)
-      flash[:success] = "Data ingestor invoked, here are the results..."
+    val = "request:"+current_user.id.to_s+":"+Time.new.utc.to_s
+    add_redis_keys
+    res = call_data_ingestor
+    if res.is_a?(Net::HTTPSuccess)
+      flash[:success] = "Data ingested, The data URL is : #{res.body}"
     else
       flash[:failure] = "Unable to get a Data URL"
       redirect_to root_path
@@ -29,6 +29,19 @@ class PagesController < ApplicationController
 
   def require_current_user
     redirect_to sign_in_path if current_user.nil?
+  end
+
+  def add_redis_keys
+    $redis.sadd("weather_machine",val)
+    $redis.sadd(val, params[:stations])
+    $redis.sadd(val, params[:start][:date])
+    $redis.sadd(val, "Sent to Data Ingestor")
+  end
+
+  def call_data_ingestor
+    uri = URI(RAIN_GAUGE[Rails.env])
+    uri.query = URI.encode_www_form(:stations => params[:stations], :date => params[:start][:date], :redis_key => val)
+    Net::HTTP.get_response(uri)
   end
 
 end
