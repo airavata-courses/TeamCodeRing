c17c099c668cdf3824f5a6ca03c53b6ebea75ffd
necsashi <necsashi@indiana.edu>
2016-10-16 23:38:13 -0400

    Registry app initial commit

---
+++ satellite/routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var redis = require('redis');
+var router = express.Router();
+var client = redis.createClient(6379,"127.0.0.1");
+
+var app = express();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  client.smembers('weather_machine', function(err, reply) {
+    var tab = "<table>";
+    for(var i = 0;i<reply.length;i++) {
+      tab += "<tr><td>"+reply[i]+"</td>";
+      client.smembers(reply[i], function(err, response) {
+        for(var j = 0;j<response.length;j++) {
+          console.log("inside second loop");
+          console.log(response);
+          tab += "<td>"+response[j]+"</td>";
+        }
+      });
+      tab += "</tr>";
+    }
+    tab += "</table>";
+    console.log(tab);
+    setTimeout(function() {res.render('index.ejs', {tab})}, 500);
+  });
+});
+
+module.exports = router;
