13cabad3d6fcaa7183b2e14591f3532128687d2b
Prashanth Murali <prashanthkumar374@gmail.com>
2016-12-21 21:09:40 +0530

    new east instance

--- anemometer/config/deploy/production.rb
+++ anemometer/config/deploy/production.rb
@@ -69,7 +69,7 @@ set :deploy_to, '/home/deploy/CodeRing'
 set :keep_releases, 1
 set :ssh_options, keys: ["config/deploy_id_rsa"] if File.exist?("config/deploy_id_rsa")
 set :use_sudo, true
-role :docker, %w{ubuntu@ec2-52-15-149-191.us-east-2.compute.amazonaws.com}, user: 'deploy'
+role :docker, %w{ec2-52-15-153-136.us-east-2.compute.amazonaws.com}, user: 'deploy'
 # server 'ubuntu@ec2-35-161-229-183.us-west-2.compute.amazonaws.com', user: 'deploy', roles: %w{docker}, :primary => true
 # server 'ubuntu@ec2-52-15-149-191.us-east-2.compute.amazonaws.com', user: 'deploy', roles: %w{docker}
 
