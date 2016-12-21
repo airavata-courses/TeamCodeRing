8cd285cdcd72260cbaf298a79818b8d71edb19bc
Prashanth Murali <prashanthkumar374@gmail.com>
2016-12-21 20:44:07 +0530

    o auth bug fix

--- anemometer/config/initializers/omniauth.rb
+++ anemometer/config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '306440530979-h2pm0ams9ugr4l7vq5ulhbt1tqd8p9oa.apps.googleusercontent.com', 'MwcwAFwrKTNYcE2Ywd7AwGGJ', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '306440530979-h2pm0ams9ugr4l7vq5ulhbt1tqd8p9oa.apps.googleusercontent.com', 'MwcwAFwrKTNYcE2Ywd7AwGGJ', {skip_jwt: true, client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
